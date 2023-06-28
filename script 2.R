bba5a3ea25f4cf010bfd803f44f8d7144ff38265
marine <marine.huchet@insee.fr>
2023-06-28 13:58:27 +0000

    version mercredi 16h

--- script 2.R
+++ script 2.R
@@ -81,6 +81,116 @@ ggplot(data = counts_PCS, aes(x=PCS, y=n))+
        y="nombre d'individus")
 
 
+#revenus mensuel du foyer par uc et niveau diplôme personne référente
+
+# Recodage des niveaux de diplôme
+
+description_indiv <- description_indiv %>% mutate(categorie_diplome=case_when(diplome_interv==1 ~ "Aucun diplôme, n'a jamais été scolarisé",
+                                                                              diplome_interv==2 ~ "Aucun diplôme, scolarité s'est arrêtée à l'école primaire",
+                                                                              diplome_interv==3 ~ "Aucun diplôme, scolarité s'est arrêtée au collège",
+                                                                              diplome_interv==4 ~ "Aucun diplôme, scolarité s'est arrêtée au delà du collège",
+                                                                              diplome_interv==5 ~ "Aucun diplôme, sans précision",
+                                                                              diplome_interv==6 ~ "CEP",
+                                                                              diplome_interv==7 ~ "CAP, BEP, BEPC, brevet élémentaire, brevet de compagnon",
+                                                                              diplome_interv==8 ~ "Baccalauréat technologique ou professionnel,\nBrevet professionnel ou de technicien,\nBEA, BEC, BEI, BEH, capacité en droit",
+                                                                              diplome_interv==9 ~ "Baccalauréat général",
+                                                                              diplome_interv==10 ~ "Diplôme de 1er cycle universitaire (Bac +3, licence),\nBTS, DUT, DEST, DEUG, diplôme des professions\nsociales ou de la santé, d'infirmier",
+                                                                              diplome_interv==11 ~ "Diplôme de 2ème cycle universitaire (Bac+4, Bac+5),\nMaster, Maîtrise, diplôme d'ingénieur,\nd'une grande école",
+                                                                              diplome_interv==12 ~ "Diplôme de 3ème cycle universitaire (>Bac+5, doctorat),\ndiplôme de vétérinaire, médecin, pharmacien",
+                                                                              diplome_interv==13 ~ "Refus",
+                                                                              diplome_interv==14 ~ "Ne sait pas"))
+
+revenu_par_diplome<-description_indiv %>%
+  group_by(categorie_diplome) %>%
+  summarise(revenu_moyen=mean(RUC_4cl,na.rm=TRUE))
+
+
+ggplot(revenu_par_diplome, aes(x=categorie_diplome, y=revenu_moyen))+
+         geom_histogram(stat="identity")+
+         coord_flip()+
+         labs(title = "Histograme du revenu par UC moyen selon le niveau de diplome",
+              x= "niveau de diplome",
+              y= "revenu par uc moyen")
+
+
+#score d'insécurité alimentaire par revenu
+description_indiv<-description_indiv %>%
+  mutate(categorie_ruc=case_when(RUC_4cl==1 ~ "<900 €/mois/UC",
+         RUC_4cl==2 ~ "[900-1 340[ €/mois/UC",
+         RUC_4cl==3 ~ "[1 340-1 850[ €/mois/U",
+         RUC_4cl==4 ~ ">=1 850 €/mois/UC"))
+
+insecurite_par_revenu<-description_indiv %>%
+  group_by(categorie_ruc) %>%
+  summarise(score_moyen=mean(IA_score,na.rm = TRUE))
+
+ggplot(insecurite_par_revenu, aes(x=categorie_ruc, y=score_moyen)) +
+  geom_histogram(stat="identity")+
+  coord_flip()+
+  labs(title="score d'insécutité alimentaire moyen par catégorie de revenu",
+       x="revenu",
+       y= "score d'insécurité alimentaire ")
+
+
+description_x_habitudes <- description_indiv %>% left_join(habitudes_indiv,by="NOIND")
+df_num <- description_x_habitudes %>% select(where(is.numeric))
+
+df_num<-df_num %>%
+  select(c("revenu","imc","agglo_5cl", "tage_PS", "IA_score", "regime_duree_sem", "poidsmax", "poidsmin"))
+
+matrice_correlation <- model.matrix(~0+., data=df_num) %>% 
+  cor(use="pairwise.complete.obs")
+       
+
+matrice_correlation %>%   
+  ggcorrplot(show.diag=FALSE, type="lower", lab=TRUE, lab_size=7)
 
 
 
+df_num2 <- description_x_habitudes %>% select(where(is.numeric))
+
+df_num2<-df_num2 %>%
+  select(c("collation_freq","revenu","imc", "tage_PS", "IA_score", "regime_duree_sem", "poidsmax", "poidsmin"))
+
+matrice_correlation2 <- model.matrix(~0+., data=df_num2) %>% 
+  cor(use="pairwise.complete.obs")
+
+
+matrice_correlation2 %>%   
+  ggcorrplot(show.diag=FALSE, type="lower", lab=TRUE, lab_size=5)
+
+
+
+
+n_rows <- nrow(habitudes_indiv)
+n_cols <- ncol(habitudes_indiv)
+print(paste("Nombre de lignes :",n_rows))
+print(paste("Nombre de colonnes :",n_cols))
+head(habitudes_indiv, 3)
+
+
+
+sum(!is.na(habitudes_indiv))/(sum(is.na(habitudes_indiv)) + sum(!is.na(habitudes_indiv)))   
+
+
+summary(habitudes_indiv)
+
+habitudes_indiv_clustering<-habitudes_indiv %>%
+  select_if(is.numeric)%>%
+  select(-c("NOIND","periode_reference"))%>%
+  select_if(function(col) any(!is.na(col))) %>%
+  select_if(function(col) length(unique(col)) > 1)
+
+Mode <- function(x) {
+  non_missing_values <- x[!is.na(x)]
+  ux <- unique(non_missing_values)
+  ux[which.max(tabulate(match(non_missing_values, ux)))]
+}
+
+habitudes_indiv_clustering_2<-habitudes_indiv_clustering %>%
+  ifelse()
+  
+  
+  
+  
+  
